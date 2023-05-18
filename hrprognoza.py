8a66ac95f5840f1e131fee8d5cd5fd4492262d90
mgracanin <35041118+mgracanin@users.noreply.github.com>
2023-05-18 13:52:10 +0200

    Create hrprognoza.py

---
+++ hrprognoza.py
@@ -0,0 +1,36 @@
+import requests
+import xmltodict
+from datetime import datetime
+
+def aktualna_prognoza():
+    '''
+    Funkcija koja vraća aktualne vremenske podatke s Vrijeme.hr (meteo.hr)
+    Vraća riječnik oblika {Grad: {Temperatura, Vlaga, Tlak, Smjer vjetra, Brzina vjetra, Vrijeme},...}
+    '''
+    url_prognoza = "https://vrijeme.hr/hrvatska1_n.xml"
+    prognoza = xmltodict.parse(requests.get(url=url_prognoza).content) ## preuzimamo i konvertiramo XML u dict
+
+    return { ## vraćamo rječnik s podacima
+            grad["GradIme"] : {
+                "Temperatura" : grad["Podatci"]["Temp"],
+                "Vlaga": grad["Podatci"]["Vlaga"],
+                "Tlak" : grad["Podatci"]["Tlak"],
+                "Smjer vjetra" : grad["Podatci"]["VjetarSmjer"],
+                "Brzina vjetra" : grad["Podatci"]["VjetarBrzina"],
+                "Vrijeme": grad["Podatci"]["Vrijeme"],
+                "Datum": datetime.now().strftime("%d. %m. %Y. %H:%M:%S")
+                } for grad in prognoza["Hrvatska"]["Grad"]
+        }
+
+
+if __name__ == "__main__":
+        
+        ## Primjeri korištenja...
+        prognoze = aktualna_prognoza()
+
+        for grad in aktualna_prognoza().keys():
+                print(f"{grad} : {prognoze[grad]}")
+
+        lista_gradova = list(aktualna_prognoza().keys())
+        print(lista_gradova)
+
