74daad551177d41a00767027aa6bb180ed410c91
Matija Gračanin <35041118+mgracanin@users.noreply.github.com>
2023-05-18 14:53:59 +0200

    Update hrprognoza.py

--- hrprognoza.py
+++ hrprognoza.py
@@ -7,20 +7,24 @@ def aktualna_prognoza():
     Funkcija koja vraća aktualne vremenske podatke s Vrijeme.hr (meteo.hr)
     Vraća riječnik oblika {Grad: {Temperatura, Vlaga, Tlak, Smjer vjetra, Brzina vjetra, Vrijeme},...}
     '''
-    url_prognoza = "https://vrijeme.hr/hrvatska1_n.xml"
-    prognoza = xmltodict.parse(requests.get(url=url_prognoza).content) ## preuzimamo i konvertiramo XML u dict
+    try:
+        url_prognoza = "https://vrijeme.hr/hrvatska1_n.xmla"
+        prognoza = xmltodict.parse(requests.get(url=url_prognoza).content) ## preuzimamo i konvertiramo XML u dict
 
-    return { ## vraćamo rječnik s podacima
-            grad["GradIme"] : {
-                "Temperatura" : grad["Podatci"]["Temp"],
-                "Vlaga": grad["Podatci"]["Vlaga"],
-                "Tlak" : grad["Podatci"]["Tlak"],
-                "Smjer vjetra" : grad["Podatci"]["VjetarSmjer"],
-                "Brzina vjetra" : grad["Podatci"]["VjetarBrzina"],
-                "Vrijeme": grad["Podatci"]["Vrijeme"],
-                "Datum": datetime.now().strftime("%d. %m. %Y. %H:%M:%S")
-                } for grad in prognoza["Hrvatska"]["Grad"]
-        }
+        return { ## vraćamo rječnik s podacima
+                grad["GradIme"] : {
+                        "Temperatura" : grad["Podatci"]["Temp"],
+                        "Vlaga": grad["Podatci"]["Vlaga"],
+                        "Tlak" : grad["Podatci"]["Tlak"],
+                        "Smjer vjetra" : grad["Podatci"]["VjetarSmjer"],
+                        "Brzina vjetra" : grad["Podatci"]["VjetarBrzina"],
+                        "Vrijeme": grad["Podatci"]["Vrijeme"],
+                        "Datum": datetime.now().timestamp()
+                        #"Datum": datetime.now().strftime("%d. %m. %Y. %H:%M:%S")
+                        } for grad in prognoza["Hrvatska"]["Grad"]
+                }
+    except Exception as e:
+          return {"Greška" : {e}}
 
 
 if __name__ == "__main__":
@@ -33,4 +37,3 @@ if __name__ == "__main__":
 
         lista_gradova = list(aktualna_prognoza().keys())
         print(lista_gradova)
-
